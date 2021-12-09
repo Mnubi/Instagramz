9579babb1fcebdc4bb66b2b7d9a58532e36729e1
mnubi <rodger.kamau@student.moringaschool.com>
2021-12-10 01:29:09 +0300

    installed requirements needed for the application

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    path('',views.homepage,name = 'index'),
+    path('search/', views.search, name='search'),
+    path('profile',views.show_profile, name='profile'),
+    path('posts/',views.new_post, name='post'),
+    path('comment/<id>', views.comment, name='comment'),
+    path('accounts/profile/',views.show_profile, name='profile'),
 ]
