2bff7439e682390ea7fff76bcb86dbb00672d66f
mnubi <rodger.kamau@student.moringaschool.com>
2021-12-10 16:22:19 +0300

    added like function to admin control

--- insta/admin.py
+++ insta/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Comment, Image,Profile
+from .models import Comment, Image, Like,Profile
 
 # Register your models here.
 admin.site.register(Image)
 admin.site.register(Profile)
-
+admin.site.register(Like)
 admin.site.register(Comment)
