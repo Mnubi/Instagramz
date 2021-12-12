1385209060d9fb54a046026d0053fd9962ffe2b9
mnubi <rodger.kamau@student.moringaschool.com>
2021-12-12 17:05:01 +0300

    deployement to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -92,7 +92,11 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'postgres',
+        'PORT':'',
     }
 }
 
